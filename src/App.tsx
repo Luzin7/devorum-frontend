c97cba8635aa52de928552f0f957c4554662650d
Luan Victor <lvictor.dutra@gmail.com>
2023-08-28 00:27:31 -0300

    feat(App.tsx): add link para repositorio do gh

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,23 @@ function App() {
   return (
     <main className="w-screen h-screen flex justify-center items-center">
       <div className="flex flex-col items-center justify-evenly h-2/6">
-        <h1 className="text-4xl font-bold underline text-primary lg:text-7xl">
+        <h1 className="text-4xl font-bold underline text-text lg:text-7xl">
           TurmaQA - Frontend
         </h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edite <code>src/App.tsx</code> e salve para testar o hmr update
         </p>
         <p className="text-sm text-center lg:text-xl">
-          Acesse o repositório e leia a documentação. Qualquer dúvida basta
-          perguntar!
+          Acesse o{' '}
+          <a
+            className="text-primary underline hover:text-accent transition-colors"
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/Luzin7/TurmaQA-Frontend"
+          >
+            repositório
+          </a>{' '}
+          e leia a documentação. Qualquer dúvida basta perguntar!
         </p>
       </div>
     </main>
