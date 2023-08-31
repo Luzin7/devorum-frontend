1c88d2154d523a76b9681cb58b78132df5364a51
Luan Victor <lvictor.dutra@gmail.com>
2023-08-31 00:03:12 -0300

    feat(main.tsx): importa roteador

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './styles/index.css';
+import { RouterProvider } from 'react-router-dom';
 
+import router from './routes/Router';
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
