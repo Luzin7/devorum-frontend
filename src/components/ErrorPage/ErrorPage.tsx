0a4a8a0b33d4895092e964c67db5267ea8d05d11
Luan Victor <lvictor.dutra@gmail.com>
2023-08-31 00:03:33 -0300

    feat(ErrorPage.tsx): cria tela de erro

---
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,31 @@
+import { type ReactElement } from 'react';
+import { Link, useRouteError } from 'react-router-dom';
+
+import { HOME } from '../../utils/routePaths';
+
+export function ErrorPage(): ReactElement {
+  const error = useRouteError() as { status: number };
+
+  const { status } = error;
+
+  return (
+    <div className="w-screen h-screen flex flex-col items-center justify-center gap-8 text-center">
+      <h1 className="text-text text-4xl font-medium lg:text-5xl">Oops!</h1>
+      <p className="text-sm w-4/5 lg:text-base">
+        Parece que você mergulhou tão fundo nas perguntas que nosso submarino de
+        conhecimento não conseguiu te alcançar! 😅 <br /> Continue curioso e
+        logo estaremos de volta com respostas ainda mais profundas! 🚀🌟
+      </p>
+      <p className="text-5xl font-bold lg:text-8xl">{status}</p>
+      <Link to={HOME}>
+        <button
+          type="button"
+          className="bg-secondary py-4 px-5 rounded-full text-text font-bold hover:text-primary hover:scale-105
+        transition-all"
+        >
+          Voltar para página inicial
+        </button>
+      </Link>
+    </div>
+  );
+}
