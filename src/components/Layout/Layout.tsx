50a702701efedef8c8c3db09057f6532d211e581
Luan Victor <lvictor.dutra@gmail.com>
2023-09-02 01:07:58 -0300

    feat(Layout.tsx): adiciona texto ao header

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,7 +4,9 @@ import { Outlet } from 'react-router-dom';
 export function Layout(): ReactElement {
   return (
     <>
-      <nav className="fixed w-screen flex items-center justify-evenly h-[7vh] top-0 text-center"></nav>
+      <nav className="fixed w-screen flex items-center justify-evenly h-[7vh] top-0 text-center">
+        Nav
+      </nav>
       <Outlet />
       <footer className="fixed w-screen flex items-center justify-center h-[20vh] bottom-0 text-center">
         Footer
