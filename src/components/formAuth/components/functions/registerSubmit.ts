7e8173a7ac33754914b0e0047d582f5f21aa221c
Luan Victor <lvictor.dutra@gmail.com>
2023-09-02 01:05:00 -0300

    feat(registerSubmit.ts): implementa funcionalidade de registro

---
+++ src/components/formAuth/components/functions/registerSubmit.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+import { type registerProps } from '../schemas';
+import { API_URL_BASE, REGISTER_ENDPOINT } from '../../../../utils/api';
+
+const handleRegisterSubmit = async (data: registerProps): Promise<object> => {
+  const { userData } = data;
+
+  try {
+    const response = await axios.post(`${API_URL_BASE}${REGISTER_ENDPOINT}`, {
+      name: userData.name,
+      contact: userData.contact,
+      password: userData.password,
+      questions: [],
+    });
+
+    if (response.status === 201) {
+      return { success: true, message: 'Usuário registrado com sucesso!' };
+    } else {
+      return {
+        success: false,
+        message: 'Ocorreu um erro ao cadastrar o usuário.',
+      };
+    }
+  } catch (error) {
+    return {
+      success: false,
+      message: error,
+    };
+  }
+};
+
+export default handleRegisterSubmit;
