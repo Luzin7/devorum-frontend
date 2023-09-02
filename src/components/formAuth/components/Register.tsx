1890d193008d1020e93a05c3aaf2f9c6e7d6475b
Luan Victor <lvictor.dutra@gmail.com>
2023-09-02 20:56:42 -0300

    refact(Register.tsx): adiciona estilos componentizados

--- src/components/formAuth/components/Register.tsx
+++ src/components/formAuth/components/Register.tsx
@@ -6,6 +6,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { LOGIN } from '../../../utils/routePaths';
 import { type registerProps, schemaRegisterForm } from './schemas';
 import { handleRegisterSubmit } from './functions';
+import * as S from './styleds';
 
 export default function Register(): ReactElement {
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -37,23 +38,18 @@ export default function Register(): ReactElement {
   };
 
   return (
-    <form
+    <S.FormWrapper
       // eslint-disable-next-line @typescript-eslint/no-misused-promises
       onSubmit={handleSubmit(onSubmit)}
-      className="flex flex-col items-center justify-evenly w-11/12  md:w-10/12 lg:w-6/12 xl:w-11/12 2xl:w-6/12
-    text-center bg-[#350C4040] rounded-3xl"
     >
       <h2 className="font-bold text-text text-2xl lg:text-3xl xl:text-4xl pt-10">
         Criar Conta
       </h2>
       <div className="flex py-7 flex-col w-11/12 lg:w-3/4">
         <div className="flex py-7 gap-7 flex-col xl:flex-row">
-          <input
+          <S.InputField
             type="text"
             placeholder="Nome"
-            className="w-full rounded-2xl py-4 px-6 placeholder:opacity-70 placeholder:text-black
-             bg-input border-transparent border-2 transition-colors focus:border-accent focus:border-2 outline-none
-              text-black bg-opacity-70"
             {...register('userData.name')}
           />
           {errors.userData?.name?.message != null && (
@@ -61,12 +57,9 @@ export default function Register(): ReactElement {
               {errors.userData?.name?.message}
             </span>
           )}
-          <input
+          <S.InputField
             type="text"
             placeholder="Usuário discord"
-            className="w-full rounded-2xl py-4 px-6 placeholder:opacity-70 placeholder:text-black
-          bg-input border-transparent border-2 transition-colors focus:border-accent focus:border-2 outline-none
-           text-black bg-opacity-70"
             {...register('userData.contact')}
           />
           {errors.userData?.contact?.message != null && (
@@ -75,12 +68,9 @@ export default function Register(): ReactElement {
             </span>
           )}
         </div>
-        <input
+        <S.InputField
           type="password"
           placeholder="Senha"
-          className="w-full rounded-2xl py-4 px-6 placeholder:opacity-70 placeholder:text-black
-          bg-input border-transparent border-2 transition-colors focus:border-accent focus:border-2 outline-none
-           text-black bg-opacity-70"
           {...register('userData.password')}
         />
         {errors.userData?.password?.message != null && (
@@ -88,18 +78,13 @@ export default function Register(): ReactElement {
             {errors.userData?.password?.message}
           </span>
         )}
+        <S.Button type="submit" disabled={!!isLoading} className="mt-7">
+          {isLoading ? 'Criando...' : 'Criar conta'}
+        </S.Button>
       </div>
-      <button
-        type="submit"
-        className="font-bold py-4 w-11/12 lg:w-3/4
-         bg-secondary rounded-full text-xl transition-colors focus:text-accent hover:text-primary"
-        disabled={!!isLoading}
-      >
-        {isLoading ? 'Criando...' : 'Criar conta'}
-      </button>
       <span className="my-8 text-primary underline text-lg hover:text-accent transition-colors">
         <Link to={LOGIN}>Já tenho uma conta</Link>
       </span>
-    </form>
+    </S.FormWrapper>
   );
 }
