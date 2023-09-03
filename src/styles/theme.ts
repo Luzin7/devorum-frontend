1c786f4c15998ef5a3cfa5908bb4d604514202f3
Luan Victor <lvictor.dutra@gmail.com>
2023-09-02 21:00:04 -0300

    style(theme.ts): cria arquivo de tema

---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+export const theme = {
+  colors: {
+    text: 'var(--text)',
+    placeholder: 'var(--placeholder)',
+    bg: 'var(--bg)',
+    primary: 'var(--primary)',
+    input: 'var(--input)',
+    secondary: 'var(--secondary)',
+    accent: 'var(--accent)',
+  },
+};
