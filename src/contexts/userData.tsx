18d7f2ab8357304dceee95f08174fde3c71fda6e
Luan Victor <lvictor.dutra@gmail.com>
2023-09-04 16:47:01 -0300

    feat(userData.tsx): implementa verificação de tema do sistema do usuario

--- src/contexts/userData.tsx
+++ src/contexts/userData.tsx
@@ -2,6 +2,10 @@ import { type ReactElement, createContext, useState } from 'react';
 
 import { type PublicUserProps } from '../types/IUser';
 
+const isUserThemeDark = window.matchMedia(
+  '(prefers-color-scheme: dark)',
+).matches;
+const userTheme = localStorage.getItem('userTheme');
 interface ProviderProp {
   children: React.ReactNode;
 }
@@ -28,6 +32,18 @@ export const UserDataProvider = ({ children }: ProviderProp): ReactElement => {
   const [userData, setUserData] = useState<PublicUserProps>(initialUser);
   const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
   const [isUserLoggedIn, setIsUserLoggedIn] = useState<boolean>(isLoggedIn);
+
+  if (isUserThemeDark) {
+    userTheme === null && localStorage.setItem('userTheme', 'dark');
+    userTheme !== 'dark' && userTheme !== null
+      ? document.documentElement.setAttribute('theme', 'light')
+      : document.documentElement.setAttribute('theme', 'dark');
+  } else {
+    userTheme === null && localStorage.setItem('userTheme', 'light');
+    userTheme !== 'light' && userTheme !== null
+      ? document.documentElement.setAttribute('theme', 'dark')
+      : document.documentElement.setAttribute('theme', 'light');
+  }
 
   return (
     <UserDataContext.Provider
