7ae28334427a3d7da7562e00675a46e3809f1eec
Luan Victor <lvictor.dutra@gmail.com>
2023-09-04 16:37:33 -0300

    feat(Layout.tsx): implementa botao para trocar de tema

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,17 @@
 import { type ReactElement } from 'react';
 import { Outlet } from 'react-router-dom';
 
+import ThemeSwitchButton from '../ThemeSwitchButton';
+
 export function Layout(): ReactElement {
   return (
     <>
-      <nav className="fixed w-screen flex items-center justify-evenly h-[7vh] top-0 text-center">
-        Nav
-      </nav>
+      <header>
+        <nav className="fixed w-screen flex items-center justify-evenly h-[7vh] top-0 text-center">
+          Nav
+          <ThemeSwitchButton />
+        </nav>
+      </header>
       <Outlet />
       <footer className="fixed w-screen flex items-center justify-center h-[20vh] bottom-0 text-center">
         Footer
