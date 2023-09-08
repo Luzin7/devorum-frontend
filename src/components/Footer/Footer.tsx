b2c493e6736f55e6a763aae9bcf13b7746ed4b21
Luan Victor <lvictor.dutra@gmail.com>
2023-09-07 22:53:55 -0300

    style(Footer.tsx): ajusta estilo

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,32 +1,20 @@
-import React, { type ReactElement } from 'react';
+import { type ReactElement } from 'react';
 import { Link } from 'react-router-dom';
-
-import { HOME } from '../../utils/routePaths';
-import logo from '../../assets/images/logo.png';
 
 export function Footer(): ReactElement {
   return (
-    <footer className="flex flex-col w-screen h-1/5 relative">
-      <div className="flex h-full w-full items-center bg-footerUp md:p-5 p-2">
-        <img src={logo} alt="Logo" className="w-[70px] h-[70px]" />
-        <ul className="flex gap-10 my-0 mx-auto sm:pr-[35px]">
-          <li>
-            <Link className="hover:opacity-50" to={HOME}>
-              Sobre nós
-            </Link>
-          </li>
-          <li>
-            <Link className="hover:opacity-50" to={HOME}>
-              Sobre o projeto
-            </Link>
-          </li>
-        </ul>
-      </div>
-      <div className="flex w-full justify-center bg-footerMid border-t-4 border-footerDown text-center p-1">
-        <p>
-          &copy; 2023 Copyright - Feito com 💖 por&nbsp;<strong>VHN</strong>
-        </p>
-      </div>
+    <footer className="bg-secondary flex flex-col h-[10vh] justify-center items-center gap-8 pt-4 text-text">
+      <ul className="flex gap-7">
+        <li className="hover:opacity-50 font-medium transition-all">
+          <Link to={'/about-us'}>Sobre nós</Link>
+        </li>
+        <li className="hover:opacity-50 font-medium transition-all">
+          <Link to={'/about-project'}>Sobre o projeto</Link>
+        </li>
+      </ul>
+      <span>
+        Feito com 💖 por <strong>VHN</strong>
+      </span>
     </footer>
   );
 }
