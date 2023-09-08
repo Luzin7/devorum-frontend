73fab40db03192bd17ec293a21a8cd42d05ea065
Luan Victor <lvictor.dutra@gmail.com>
2023-09-07 22:59:52 -0300

    style(Button.ts): adicionar cor ao texto e ajusta tamanho

--- src/components/ThemeSwitchButton/components/styleds/Button.ts
+++ src/components/ThemeSwitchButton/components/styleds/Button.ts
@@ -1,9 +1,15 @@
 import styled from 'styled-components';
 
 const Button = styled.button`
-  width: 50px;
-  height: 50px;
+  color: ${(props) => props.theme.colors.text};
+  font-size: 7vh;
   border-radius: 100%;
+
+  & {
+    @media (min-width: 1024px) {
+      font-size: 4vh;
+    }
+  }
 `;
 
 export default Button;
