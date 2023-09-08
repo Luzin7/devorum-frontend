520a139be4ecae2584280ee452c60be6299f8a43
Luan Victor <lvictor.dutra@gmail.com>
2023-09-07 22:53:26 -0300

    style(ErrorPage.tsx): ajusta cores

--- src/components/ErrorPage/ErrorPage.tsx
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -11,16 +11,16 @@ export function ErrorPage(): ReactElement {
   return (
     <div className="w-screen h-screen flex flex-col items-center justify-center gap-8 text-center">
       <h1 className="text-text text-4xl font-medium lg:text-5xl">Oops!</h1>
-      <p className="text-sm w-4/5 lg:text-base">
+      <p className="text-text text-sm w-4/5 lg:text-base">
         Parece que você mergulhou tão fundo nas perguntas que nosso submarino de
         conhecimento não conseguiu te alcançar! 😅 <br /> Continue curioso e
         logo estaremos de volta com respostas ainda mais profundas! 🚀🌟
       </p>
-      <p className="text-5xl font-bold lg:text-8xl">{status}</p>
+      <p className="text-text text-5xl font-bold lg:text-8xl">{status}</p>
       <Link to={HOME}>
         <button
           type="button"
-          className="bg-secondary py-4 px-5 rounded-full text-text font-bold hover:text-primary hover:scale-105
+          className="bg-accent py-4 px-5 rounded-full text-text font-bold hover:bg-primary hover:scale-105
         transition-all"
         >
           Voltar para página inicial
