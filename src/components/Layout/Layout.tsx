8c4a4247b3feab0683f13d3d0f29bb3e93924d83
Luan Victor <lvictor.dutra@gmail.com>
2023-09-07 22:56:41 -0300

    refact(Layout.tsx): transforma header e footer em componentes

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,21 +1,15 @@
 import { type ReactElement } from 'react';
 import { Outlet } from 'react-router-dom';
 
-import ThemeSwitchButton from '../ThemeSwitchButton';
+import Header from '../Header';
+import Footer from '../Footer';
 
 export function Layout(): ReactElement {
   return (
     <>
-      <header>
-        <nav className="flex items-center justify-evenly h-[7vh]">
-          Nav
-          <ThemeSwitchButton />
-        </nav>
-      </header>
+      <Header />
       <Outlet />
-      <footer className="flex items-center justify-center h-[7vh]">
-        Footer
-      </footer>
+      <Footer />
     </>
   );
 }
