7af9319e8a7e5ff17eb2f2b3b19ec75afe79c8c2
Luan Victor <lvictor.dutra@gmail.com>
2023-09-11 16:03:56 -0300

    feat(index.ts): add novas importações

---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+import { type UserProps, type PublicUserProps } from './IUser';
+import type QuestionProps from './IQuestion';
+import type CommentProps from './IComment';
+
+export type { UserProps, PublicUserProps, QuestionProps, CommentProps };
