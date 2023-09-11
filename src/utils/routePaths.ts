cc1c6a9d2e398a74fe43afceb64574269653a91a
Luan Victor <lvictor.dutra@gmail.com>
2023-09-11 16:09:01 -0300

    feat(routePaths.ts): cria e exporta caminhos

--- src/utils/routePaths.ts
+++ src/utils/routePaths.ts
@@ -1,5 +1,5 @@
 export const ROOT: string = '/';
 export const HOME: string = '/home';
-export const QUESTION: string = '/question';
+export const QUESTION: string = '/questions';
 export const LOGIN: string = '/login';
 export const REGISTER: string = '/register';
