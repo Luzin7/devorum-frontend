b106f897fc6bb335c6656e1f2aec301338617a9b
Luan Victor <lvictor.dutra@gmail.com>
2023-09-12 13:36:09 -0300

    refact(AboutUsCard.tsx): cria componente de cards sobre nos

---
+++ src/components/AboutUsCards/AboutUsCards.tsx
@@ -0,0 +1,7 @@
+import { type ReactElement } from 'react';
+
+import { PersonCardList } from './components/PersonCardList';
+
+export function AboutUsCards(): ReactElement {
+  return <PersonCardList />;
+}
