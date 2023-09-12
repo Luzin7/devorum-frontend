7f3552dbfccd12222231eeeb740fff3c04a2f6f1
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-09-12 17:28:23 -0300

    update(routePath.ts) exporta rota

--- src/utils/routePaths.ts
+++ src/utils/routePaths.ts
@@ -4,3 +4,4 @@ export const QUESTION: string = '/questions';
 export const LOGIN: string = '/login';
 export const REGISTER: string = '/register';
 export const ABOUT_US: string = '/about-us';
+export const ABOUT_PROJECT: string = '/about-project';
