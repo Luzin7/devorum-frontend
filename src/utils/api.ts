f0e4104a75e8b149e73230d925def51c395d0cb3
Luan Victor <lvictor.dutra@gmail.com>
2023-09-16 17:54:48 -0300

    bomb commit

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 export const API_URL_BASE = process.env.API_URL_BASE;
 export const LOGIN_ENDPOINT = process.env.LOGIN_ENDPOINT;
 export const REGISTER_ENDPOINT = process.env.REGISTER_ENDPOINT;
