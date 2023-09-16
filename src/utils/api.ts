74ed3838fa54f0fffc8298aedd16e14754398dfe
Luan Victor <lvictor.dutra@gmail.com>
2023-09-16 18:29:31 -0300

    bomb commit

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,9 @@
-import 'dotenv/config';
 
-export const API_URL_BASE = process.env.API_URL_BASE;
-export const LOGIN_ENDPOINT = process.env.LOGIN_ENDPOINT;
-export const REGISTER_ENDPOINT = process.env.REGISTER_ENDPOINT;
-export const QUESTIONS_ENDPOINT = process.env.QUESTIONS_ENDPOINT;
-export const COMMENTS_ENDPOINT = process.env.COMMENTS_ENDPOINT;
+export const API_URL_BASE = import.meta.env.VITE_REACT_APP_API_URL_BASE;
+export const LOGIN_ENDPOINT = import.meta.env.VITE_REACT_APP_LOGIN_ENDPOINT;
+export const REGISTER_ENDPOINT = import.meta.env
+  .VITE_REACT_APP_REGISTER_ENDPOINT;
+export const QUESTIONS_ENDPOINT = import.meta.env
+  .VITE_REACT_APP_QUESTIONS_ENDPOINT;
+export const COMMENTS_ENDPOINT = import.meta.env
+  .VITE_REACT_APP_COMMENTS_ENDPOINT;
