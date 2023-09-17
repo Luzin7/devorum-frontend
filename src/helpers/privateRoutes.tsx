c026d4ca84a551b9cc2e7e90add62374e4ec35e3
Luan Victor <lvictor.dutra@gmail.com>
2023-09-17 00:20:55 -0300

    refact(privateRoutes.tsx): otimiza redirecionamento de rotas

--- src/helpers/privateRoutes.tsx
+++ src/helpers/privateRoutes.tsx
@@ -1,21 +1,11 @@
 import { type ReactElement, useContext } from 'react';
-import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 
-import { HOME, LOGIN, ROOT } from '../utils/routePaths';
+import { LOGIN } from '../utils/routePaths';
 import { UserDataContext } from '../contexts/userData';
 
 export const PrivateRoutes = (): ReactElement => {
   const { isUserLoggedIn } = useContext(UserDataContext);
-  const currentLocation = useLocation().pathname;
 
-  return !isUserLoggedIn ? (
-    <>
-      <Navigate to={LOGIN} />
-      <Outlet />
-    </>
-  ) : currentLocation === ROOT ? (
-    <Navigate to={HOME} />
-  ) : (
-    <Outlet />
-  );
+  return isUserLoggedIn ? <Outlet /> : <Navigate to={LOGIN} />;
 };
