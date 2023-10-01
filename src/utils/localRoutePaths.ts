0154b35c2e9a758c526d8bf6b614875b8c2e2be2
Luan Victor <lvictor.dutra@gmail.com>
2023-10-01 16:24:08 -0300

    update: altera rota de perfil de usuario

--- src/utils/localRoutePaths.ts
+++ src/utils/localRoutePaths.ts
@@ -3,6 +3,6 @@ export const TOPIC = '/:authorName/:topicTitle';
 export const LOGIN = '/login';
 export const REGISTER = '/register';
 export const ABOUT_US = '/about-us';
-export const USER_PROFILE = '/profile/:userName';
+export const USER_PROFILE = '/profile';
 export const CREATE_TOPIC = '/post';
 export const ABOUT_PROJECT = '/about-project';
