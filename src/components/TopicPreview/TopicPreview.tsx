773f3e4bca6bbf6033d301f0e6294ba8d267ad99
Luan Victor <lvictor.dutra@gmail.com>
2023-10-01 00:54:51 -0300

    feat: passa conteudo formatado para html

--- src/components/TopicPreview/TopicPreview.tsx
+++ src/components/TopicPreview/TopicPreview.tsx
@@ -1,33 +1,35 @@
-import { dateConverter, reduceText, slugUrlMaker } from 'functions'
-import Link from 'next/link'
-import React from 'react'
-import Topic from 'types'
+import { dateConverter, reduceText, slugUrlMaker } from 'functions';
+import Link from 'next/link';
+import React from 'react';
+import Topic from 'types';
 
 // TROCAR O BOTAO E DEIXAR A PROPRIA PERGUNTA SER UM LINK?
+// PEGAR O JSON DO TITAP E USAR COIMO PREVIEW PAPAI
 
 type TopicPreviewProps = Pick<
   Topic,
   'title' | 'content' | 'authorName' | 'createdAt'
->
+>;
 
 export function TopicPreview({
   title,
   content,
   authorName,
-  createdAt,
+  createdAt
 }: TopicPreviewProps) {
   return (
     <>
       <div>
         <div className="flex flex-col gap-1">
-          <p className="text-background opacity-70 text-sm">
+          <p className="text-text opacity-70 text-sm">
             {reduceText(title, 25)}
           </p>
-          <p className="text-primary font-semibold">
-            {reduceText(content, 45)}
-          </p>
+          <div
+            className="font-bold text-text"
+            dangerouslySetInnerHTML={{ __html: reduceText(content, 50) }}
+          ></div>
         </div>
-        <p className="text-background opacity-50 text-xs mt-2 2xl:mt-5">
+        <p className="text-text opacity-40 text-xs mt-2 2xl:mt-5">
           {dateConverter(createdAt)}
         </p>
       </div>
@@ -35,12 +37,12 @@ export function TopicPreview({
         <Link href={`topic/${authorName}/${slugUrlMaker(title)}`}>
           <button
             type="button"
-            className="font-bold bg-accent rounded-full py-2 px-4 text-sm hover:bg-primary transition-colors"
+            className="font-bold bg-accent text-blue-100 rounded-full py-2 px-4 text-sm hover:bg-primary transition-colors"
           >
             Conferir
           </button>
         </Link>
       </div>
     </>
-  )
+  );
 }
