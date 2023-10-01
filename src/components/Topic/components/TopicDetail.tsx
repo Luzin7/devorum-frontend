bd55fc51c24d863a2a9bea29240fdf5cf2b0657c
Luan Victor <lvictor.dutra@gmail.com>
2023-10-01 16:21:58 -0300

    em andamento

--- src/components/Topic/components/TopicDetail.tsx
+++ src/components/Topic/components/TopicDetail.tsx
@@ -14,13 +14,13 @@ export default function TopicDetail({
   title
 }: TopicDetailsProps) {
   return (
-    <div>
+    <div className="prose prose-strong:text-text prose-code:text-text prose-code:bg-secondary prose-code:rounded-sm m-auto text-text">
       <div>
         <span>
           {authorName} {dateConverter(createdAt)}
         </span>
       </div>
-      <h1 className="text-2xl font-bold">{title}</h1>
+      <h1 className="text-2xl font-bold text-text">{title}</h1>
       <div dangerouslySetInnerHTML={{ __html: content }}></div>
     </div>
   );
