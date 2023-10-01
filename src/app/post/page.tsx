b3a4401bd7370c12bd1f385acad2a2cab264575c
Luan Victor <lvictor.dutra@gmail.com>
2023-10-01 16:16:01 -0300

    refact: retira comentarios e funções

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,12 +1,8 @@
 'use client';
 
-// ADICIONAR ZOD PARA TITULO
-
 import Editor from '@components/Editor/Editor';
-import { useTopicStore } from 'store/topic';
 
 export default function CreateTopic() {
-  const { state } = useTopicStore();
   return (
     <div className="w-11/12 m-auto pt-4 lg:w-2/4">
       <div className="flex flex-col gap-2">
@@ -17,12 +13,6 @@ export default function CreateTopic() {
         </p>
       </div>
       <Editor />
-      <button
-        className="mt-4 py-4 w-full bg-accent rounded-md"
-        onClick={() => console.log(state.topic)}
-      >
-        Publicar
-      </button>
     </div>
   );
 }
