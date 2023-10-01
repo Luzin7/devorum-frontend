afa66386b3d02646bf827fb0f079e5b495bc858a
Luan Victor <lvictor.dutra@gmail.com>
2023-10-01 16:23:01 -0300

    feat: cria componentes estilizados

---
+++ src/styles/globalStyledComponents.tsx
@@ -0,0 +1,29 @@
+import { Ref, forwardRef } from 'react';
+import { ButtonProps } from 'types/globalStyledComponents';
+
+export const Error = ({ errorMessage }: { errorMessage: string }) => {
+  return <span className="text-red-400">{errorMessage}</span>;
+};
+
+export const Button = forwardRef(function Button(
+  {
+    isLoading = false,
+    bgColor = 'secondary',
+    txtColor = 'text',
+    txtHoverColor = 'primary',
+    ...props
+  }: ButtonProps,
+  ref: Ref<HTMLButtonElement>
+) {
+  return (
+    <button
+      {...props}
+      ref={ref}
+      className={`bg-${bgColor} mt-7 font-bold py-4 min-w-full rounded-lg text-xl text-${txtColor} hover:text-${txtHoverColor}${
+        isLoading && 'animate-pulse'
+      }`}
+    ></button>
+  );
+});
+
+Button.displayName = 'Button';
