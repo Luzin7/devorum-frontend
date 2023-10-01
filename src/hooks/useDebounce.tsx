c78b6e78aadea3dc8989bea5af9335fce56229d2
Luan Victor <lvictor.dutra@gmail.com>
2023-10-01 00:43:05 -0300

    feat: cria hook personalizado para delay de ação

---
+++ src/hooks/useDebounce.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react';
+
+export function useDebounce(value: string, delay: number) {
+  const [debounceValue, setDebounceValue] = useState('');
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebounceValue(value);
+    }, delay);
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debounceValue;
+}
