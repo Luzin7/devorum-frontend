5feea94715779b486154d5d6b43779896378a718
Luan Victor <lvictor.dutra@gmail.com>
2023-10-01 00:40:00 -0300

    feat: cria indexados para exportar modulos

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,6 +1,15 @@
-import { reduceText } from './reduceText'
-import { dateConverter } from './dateConverter'
-import { reducePreviewText } from './reducePreviewText'
-import { slugUrlMaker } from './slugUrlMaker'
+import { reduceText } from './reduceText';
+import { dateConverter } from './dateConverter';
+import { slugUrlMaker } from './slugUrlMaker';
+import { setLocalStorage } from 'functions/setLocalStorage';
+import { getLocalStorage } from './getLocalStorage';
+import { themeSwitcher } from './themeSwitcher';
 
-export { reduceText, dateConverter, reducePreviewText, slugUrlMaker }
+export {
+  reduceText,
+  dateConverter,
+  slugUrlMaker,
+  setLocalStorage,
+  getLocalStorage,
+  themeSwitcher
+};
