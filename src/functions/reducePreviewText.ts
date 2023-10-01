361f2c946ec3232a9225412987a9b6c90473964d
Luan Victor <lvictor.dutra@gmail.com>
2023-10-01 00:32:06 -0300

    remove

--- src/functions/reducePreviewText.ts
+++
@@ -1,8 +0,0 @@
-export function reducePreviewText(text: string, maxLenght: number) {
-  // adicionar logica para tamanhos diferentes de tela
-  if (text.length <= maxLenght) {
-    return text
-  } else {
-    return text.slice(0, maxLenght).trim() + '...'
-  }
-}
