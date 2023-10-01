f2a977dca2d3e8efb76bdf57f0e5a817e416a3e5
Luan Victor <lvictor.dutra@gmail.com>
2023-10-01 00:33:22 -0300

    style: correção de linter

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,27 @@
-'use client'
+'use client';
 
-import Logo from '@components/Logo'
-import React, { useState } from 'react'
-import { CREATE_TOPIC, USER_PROFILE } from 'utils'
-import { BiMessageAltAdd, BiSolidUserRectangle } from 'react-icons/bi'
-import { Dropdown } from '@components/Dropdown'
-import Link from 'next/link'
-import { getThemeBySystem } from 'functions/getUserSystemTheme'
+import Logo from '@components/Logo';
+import React, { useState } from 'react';
+import { CREATE_TOPIC } from 'utils';
+import { BiMessageAltAdd, BiSolidUserRectangle } from 'react-icons/bi';
+import { Dropdown } from '@components/Dropdown';
+import Link from 'next/link';
+import { getThemeBySystem } from 'functions/getUserSystemTheme';
 
 export function Header() {
-  getThemeBySystem()
-  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false)
+  getThemeBySystem();
+  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
 
   const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen)
-  }
+    setIsDropdownOpen(!isDropdownOpen);
+  };
   return (
     <header className="flex bg-secondary gap-4 items-center justify-between h-[7vh] px-4 text-whiteText">
       {isDropdownOpen && (
         <Dropdown.Root>
           <Dropdown.Links>
+            <Dropdown.Link href={`/profile/Luzin`} title="Meu perfil" />
+            <hr />
             <Dropdown.Link href={CREATE_TOPIC} title="Publicar novo tópico" />
             <Dropdown.Link href={CREATE_TOPIC} title="Editar perfil" />
             <Dropdown.Link href={CREATE_TOPIC} title="Minhas publicações" />
@@ -41,5 +43,5 @@ export function Header() {
         />
       </div>
     </header>
-  )
+  );
 }
