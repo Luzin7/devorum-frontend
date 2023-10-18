d6e61a26baecc7e02c9d6dc68d76cb4e4d777d31
Luan Victor <lvictor.dutra@gmail.com>
2023-10-18 19:28:20 -0300

    update: melhora estrutura de tipagem

--- src/types/IComments.ts
+++ src/types/IComments.ts
@@ -1,7 +1,22 @@
+import { UUID } from 'crypto';
+
 export interface Comment {
   topicId: string;
   authorId: string;
   content: string;
   createdAt: Date;
   updatedAt: Date | null;
+}
+
+export interface CommentAuthor {
+  id: UUID;
+  name: string;
+}
+
+export interface CommentWithAuthor {
+  id: UUID;
+  content: string;
+  createdAt: Date;
+  updatedAt: null | Date;
+  author: CommentAuthor[];
 }
