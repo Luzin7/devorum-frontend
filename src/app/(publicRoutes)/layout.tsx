ebb9bf4c09e749863ad694258c4aac28210421cf
Luan Victor <lvictor.dutra@gmail.com>
2023-10-18 19:26:01 -0300

    feat: implementa persistencia de autenticacao (em andamento)

---
+++ src/app/(publicRoutes)/layout.tsx
@@ -0,0 +1,12 @@
+'use client';
+import { PersistAuth } from 'functions';
+
+export default function PrivateLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  PersistAuth();
+
+  return <>{children}</>;
+}
