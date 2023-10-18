0a441ce237baccb5d7f3bb89fc95c7e0669351af
Luan Victor <lvictor.dutra@gmail.com>
2023-10-18 19:30:04 -0300

    update: adiciona lógica booleana

--- src/components/Dropdown/composition/DropdownAction.tsx
+++ src/components/Dropdown/composition/DropdownAction.tsx
@@ -1,10 +1,20 @@
+import { useRouter } from 'next/navigation';
+import { LOGIN } from 'utils';
 interface DropdownActionProps {
   btnTitle?: string;
 }
 
 export function DropdownAction({ btnTitle = 'Sair' }: DropdownActionProps) {
+  const router = useRouter();
+
   return (
-    <button type="button" className="text-left text-red-400">
+    <button
+      type="button"
+      className="text-left text-red-400"
+      onClick={() =>
+        btnTitle === 'Sair' ? 'Not implemented' : router.push(LOGIN)
+      }
+    >
       {btnTitle}
     </button>
   );
