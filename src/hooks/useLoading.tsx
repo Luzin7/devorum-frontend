f71d84f3b77a52ea09f063b6729e5e54246cdccc
Luan Victor <lvictor.dutra@gmail.com>
2023-10-21 17:04:43 -0300

    feat: adiciona estado de sucesso ou falha

--- src/hooks/useLoading.tsx
+++ src/hooks/useLoading.tsx
@@ -2,9 +2,12 @@ import { useState } from 'react';
 
 export const useLoading = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
 
   return {
     isLoading,
-    setIsLoading
+    setIsLoading,
+    isSuccess,
+    setIsSuccess
   };
 };
