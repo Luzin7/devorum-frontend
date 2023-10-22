bcb3939a78562575742a5740e7ac8cbcb6c28cd1
Luan Victor <lvictor.dutra@gmail.com>
2023-10-22 13:18:16 -0300

    update: corrije erro ortográfico

--- src/components/AboutProject/data/questions/index.ts
+++ src/components/AboutProject/data/questions/index.ts
@@ -2,7 +2,7 @@ export const items = [
   {
     title: 'O que é o Devorum?',
     content:
-      'Uma plataforma de perguntas e respostas para atender às necessidades de quem precisa. A plataforma permitire que façam perguntas, obtenham ajuda e interajam em um ambiente de aprendizado colaborativo.'
+      'Uma plataforma de perguntas e respostas para atender às necessidades de quem precisa. A plataforma permite que façam perguntas, obtenham ajuda e interajam em um ambiente de aprendizado colaborativo.'
   },
   {
     title: 'O que preciso fazer para participar?',
