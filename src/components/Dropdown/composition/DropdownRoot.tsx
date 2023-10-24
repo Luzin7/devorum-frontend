f6040ec717ce110aaef7983da27d478fd2044ae8
Luan Victor <lvictor.dutra@gmail.com>
2023-10-24 15:53:31 -0300

    refact: implementa lógica para root

--- src/components/Dropdown/composition/DropdownRoot.tsx
+++ src/components/Dropdown/composition/DropdownRoot.tsx
@@ -1,13 +1,9 @@
-import { ReactNode } from 'react';
+import { HtmlHTMLAttributes, ReactNode } from 'react';
 
-interface DropdownRootProps {
+interface DropdownRootProps extends HtmlHTMLAttributes<HTMLDivElement> {
   children: ReactNode;
 }
 
-export function DropdownRoot({ children }: DropdownRootProps) {
-  return (
-    <div className="absolute z-50 flex flex-col right-2 top-[6vh] bg-secondary rounded-xl w-2/4 md:w-1/4 lg:w-1/6 xl:w-[10%] py-2 px-4">
-      {children}
-    </div>
-  );
+export function DropdownRoot({ children, ...props }: DropdownRootProps) {
+  return <div {...props}>{children}</div>;
 }
