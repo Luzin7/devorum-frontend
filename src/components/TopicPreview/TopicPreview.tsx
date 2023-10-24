d18a907f6c9502827eddd11312ca5c82565117a1
Luan Victor <lvictor.dutra@gmail.com>
2023-10-24 15:57:29 -0300

    style: adicionar maior visualização de texto

--- src/components/TopicPreview/TopicPreview.tsx
+++ src/components/TopicPreview/TopicPreview.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import { TopicPreviewProps } from 'types/ITopic';
 
 // TROCAR O BOTAO E DEIXAR A PROPRIA PERGUNTA SER UM LINK?
-// PEGAR O JSON DO TITAP E USAR COIMO PREVIEW PAPAI
 
 type TopicProps = Pick<
   TopicPreviewProps,
@@ -23,7 +22,7 @@ export function TopicPreview({
       <div>
         <div className="flex flex-col gap-1">
           <p className="text-text opacity-70 text-sm">{title}</p>
-          <p className="font-bold text-text">{reduceText(assertion, 45)}</p>
+          <p className="font-bold text-text">{reduceText(assertion, 100)}</p>
         </div>
         <p className="text-text opacity-40 text-xs mt-2 2xl:mt-5">
           {dateConverter(createdAt)}
