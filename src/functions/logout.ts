61e1fbee43591f275006fcb6a4e39c9f79eb82a1
Luan Victor <lvictor.dutra@gmail.com>
2023-10-24 15:52:16 -0300

    feat: cria função de logout

---
+++ src/functions/logout.ts
@@ -0,0 +1,21 @@
+import httpClient from 'services/http/requests/axios-conf';
+
+function clearCookies() {
+  const cookies = document.cookie.split('; ');
+
+  for (const cookie of cookies) {
+    const [name] = cookie.split('=');
+    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
+  }
+}
+
+export function logout() {
+  httpClient.defaults.headers.Authorization = null;
+  httpClient.defaults.headers.refresh_token = null;
+
+  clearCookies();
+
+  localStorage.clear();
+
+  window.location.reload();
+}
