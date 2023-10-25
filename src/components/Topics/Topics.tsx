b5d1931cbae700ece1615bf39d5ce8fd09e283ef
Luan Victor <lvictor.dutra@gmail.com>
2023-10-25 03:47:53 -0300

    style: adiciona mais informações no preview

--- src/components/Topics/Topics.tsx
+++ src/components/Topics/Topics.tsx
@@ -6,20 +6,23 @@ export async function Topics() {
 
   return (
     <div className="py-6 flex flex-col gap-4 w-full max-h-[90vh] overflow-y-scroll rounded-2xl cursor-default md:m-auto lg:w-3/5 xl:w-2/4">
-      {topics.map(({ assertion, createdAt, title, id, author }) => (
-        <div
-          className="bg-topicBackground flex justify-between p-2 rounded-md md:p-4 md:rounded-xl"
-          key={id}
-        >
-          <TopicPreview
-            id={id}
-            author={author}
-            assertion={assertion}
-            createdAt={createdAt}
-            title={title}
-          />
-        </div>
-      ))}
+      {topics.map(
+        ({ assertion, createdAt, title, id, author, numberOfComments }) => (
+          <div
+            className="bg-topicBackground flex justify-between p-2 rounded-md md:p-4 md:rounded-xl"
+            key={id}
+          >
+            <TopicPreview
+              id={id}
+              author={author}
+              assertion={assertion}
+              createdAt={createdAt}
+              title={title}
+              numberOfComments={numberOfComments}
+            />
+          </div>
+        )
+      )}
     </div>
   );
 }
