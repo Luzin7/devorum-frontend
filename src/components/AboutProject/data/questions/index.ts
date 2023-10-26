b0e93a2be2c8e5e25422e7e5cd5e6f0972279bc7
Luan Victor <lvictor.dutra@gmail.com>
2023-10-26 16:21:48 -0300

    feat: atualiza lista de FAQ

--- src/components/AboutProject/data/questions/index.ts
+++ src/components/AboutProject/data/questions/index.ts
@@ -10,8 +10,13 @@ export const items = [
       'Basta acessar! Você pode acessar tudo, sem restrições. Se quiser participar ainda mais compartilhando ideias, dúvidas ou respostas, basta criar sua conta!'
   },
   {
-    title: 'Descobri um problema ou um possível erro, como posso ajudar?',
+    title: 'Descobri um problema ou um possível erro crítico, onde reporto?',
     content:
-      'Se sinta livre para compartilhar conosco sua descoberta pelo discord: lvictor7'
+      'Em breve teremos um local mais apropriado para isso, porém, se sinta livre para compartilhar conosco sua descoberta pelo discord: lvictor7'
+  },
+  {
+    title: 'Como posso colaborar com o projeto?',
+    content:
+      'Ainda estamos em fase de desenvolvimento e, portanto, nosso código fonte ainda não é aberto para todos colaborarem. No entanto, ficamos felizes em saber que você se interessa em colaborar com esse projeto!'
   }
 ];
