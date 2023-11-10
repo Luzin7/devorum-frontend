5312b6266f1d880a7ad9ee07362ef9f6b5aff4c1
Luan Victor <lvictor.dutra@gmail.com>
2023-11-10 12:01:28 -0300

    feat: torna preview um link

--- src/components/Topics/Topics.tsx
+++ src/components/Topics/Topics.tsx
@@ -1,4 +1,6 @@
 import TopicPreview from '@components/TopicPreview';
+import { slugUrlMaker } from 'functions';
+import Link from 'next/link';
 import { getTopics } from 'services/http/requests/api';
 
 export async function Topics() {
@@ -8,19 +10,20 @@ export async function Topics() {
     <div className="pt-2 flex flex-col gap-4 w-full max-h-[86vh] overflow-y-scroll rounded-2xl cursor-default md:m-auto lg:w-3/5 xl:w-2/4">
       {topics.map(
         ({ assertion, createdAt, title, id, author, numberOfComments }) => (
-          <div
-            className="bg-topicBackground flex justify-between p-2 rounded-md md:p-4 md:rounded-xl"
+          <Link
+            href={`topic/${slugUrlMaker(author.name)}/${id.replace('%7D', '')}`}
             key={id}
           >
-            <TopicPreview
-              id={id}
-              author={author}
-              assertion={assertion}
-              createdAt={createdAt}
-              title={title}
-              numberOfComments={numberOfComments}
-            />
-          </div>
+            <div className="bg-topicBackground flex justify-between p-2 rounded-md md:p-4 md:rounded-xl">
+              <TopicPreview
+                author={author}
+                assertion={assertion}
+                createdAt={createdAt}
+                title={title}
+                numberOfComments={numberOfComments}
+              />
+            </div>
+          </Link>
         )
       )}
     </div>
