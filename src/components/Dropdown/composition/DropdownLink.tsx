0666d6c24a0decd42a06ed1ba548a1b327fa9537
Luzin7 <lvictor.dutra@gmail.com>
2024-06-30 19:06:58 -0300

    chore: remove

--- src/components/Dropdown/composition/DropdownLink.tsx
+++
@@ -1,8 +0,0 @@
-import LinkItem from '@components/LinkItem';
-import React, { ComponentProps } from 'react';
-
-type DropdownLinkProps = ComponentProps<typeof LinkItem>;
-
-export function DropdownLink({ title, ...props }: DropdownLinkProps) {
-  return <LinkItem title={title} {...props} />;
-}
--- src/components/Dropdown/composition/DropdownRoot.tsx
+++
@@ -1,9 +0,0 @@
-import { HtmlHTMLAttributes, ReactNode } from 'react';
-
-interface DropdownRootProps extends HtmlHTMLAttributes<HTMLDivElement> {
-  children: ReactNode;
-}
-
-export function DropdownRoot({ children, ...props }: DropdownRootProps) {
-  return <div {...props}>{children}</div>;
-}
